a3363d5df9353d4f8946c9b5dcf045daebe57880
GDeDev <giulianodamico@gmail.com>
2021-11-04 23:27:35 -0300

    Primer componente

--- config/database.js
+++ config/database.js
@@ -1,36 +1,15 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: "default",
-//   connections: {
-//     default: {
-//       connector: "mongoose",
-//       settings: {
-//         host: env("DATABASE_HOST"),
-//         srv: env.bool("DATABASE_SRV", true),
-//         port: env.int("DATABASE_PORT", 27017),
-//         database: env("DATABASE_NAME"),
-//         username: env("DATABASE_USERNAME"),
-//         password: env("DATABASE_PASSWORD"),
-//       },
-//       options: {
-//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-//         ssl: env.bool("DATABASE_SSL", true),
-//       },
-//     },
-//   },
-// });
-
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "cluster0.fgeuy.mongodb.net"),
+        host: env("DATABASE_HOST"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", ""),
-        username: env("DATABASE_USERNAME", "gdedev"),
-        password: env("DATABASE_PASSWORD", "Monubarapisarda18"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
@@ -39,3 +18,24 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "mongoose",
+//       settings: {
+//         host: env("DATABASE_HOST", "cluster0.fgeuy.mongodb.net"),
+//         srv: env.bool("DATABASE_SRV", true),
+//         port: env.int("DATABASE_PORT", 27017),
+//         database: env("DATABASE_NAME", ""),
+//         username: env("DATABASE_USERNAME", "gdedev"),
+//         password: env("DATABASE_PASSWORD", "Monubarapisarda18"),
+//       },
+//       options: {
+//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+//         ssl: env.bool("DATABASE_SSL", true),
+//       },
+//     },
+//   },
+// });
